9cc9e8ff57c6e82a4e040247dbe0e96cfb2a0c26
Unigmos <77985354+Unigmos@users.noreply.github.com>
2021-07-05 19:15:43 +0900

    Add files via upload

---
+++ 復号化用/SimpleDecryption.py
@@ -0,0 +1,17 @@
+'''ここの中身を書き換える'''
+inputpass = 'input.txt'   #入力ファイル名
+outputname = 'output.txt' #出力ファイル名
+shift = 3                 #ずらす文字数
+
+'''ここより下はいじる必要はありません'''
+encryption = ''
+
+textdata = open(inputpass)
+lines = textdata.readlines()
+textdata.close()
+
+with open (outputname, 'x') as output:
+    for data in lines:
+        for f in data:
+            encryption = chr(ord(f) - shift)
+            output.write(encryption)
